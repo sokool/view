19bfadb27b3a6778221a6db4ecbc3fae2c2c0651
Michał Sokołowski <michal.sokolowski@inflect.com>
2024-10-07 12:25:25 +0200

    json decoder + reader with additional methods

--- meta.go
+++ meta.go
@@ -1,5 +1,10 @@
 package view
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 func Write(name string, v any) Writer { return Writer{name: v} }
 
 type Writer map[string]any
@@ -7,4 +12,13 @@ type Writer map[string]any
 func (w Writer) Write(name string, v any) Writer {
 	w[name] = v
 	return w
+}
+
+func (w Writer) GoString() string {
+	return stringify(w)
+}
+
+func stringify(v any) string {
+	b, _ := json.MarshalIndent(v, "", "\t")
+	return fmt.Sprintf("%T\n%s\n", v, b)
 }
